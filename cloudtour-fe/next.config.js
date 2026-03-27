5225ed73e923fc1ed410aa5bdb6ca1d1dc3adcb0
Zhang Zehua <edward.zehua.zhang@gmail.com>
2026-03-27 15:35:20 +0800

    fix: allow Supabase Storage images in Next.js remotePatterns

--- cloudtour-fe/next.config.js
+++ cloudtour-fe/next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@cloudtour/ui", "@cloudtour/db", "@cloudtour/types"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "siwkrxtdijvutuerunzv.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
