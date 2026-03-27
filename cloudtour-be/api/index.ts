2b47f719c405fc55b03d50d1dcb53b9fd7543f9c
Zhang Zehua <edward.zehua.zhang@gmail.com>
2026-03-27 16:20:20 +0800

    fix: fix Vercel deployment of cloudtour-be (hono/vercel adapter, split app.ts)

--- cloudtour-be/api/index.ts
+++ cloudtour-be/api/index.ts
@@ -1,4 +1,5 @@
-import { handle } from "@hono/vercel";
-import { app } from "../src/index";
+﻿import { handle } from "hono/vercel";
+import { app } from "../src/app";
 
+export const config = { runtime: "nodejs20.x" };
 export default handle(app);
